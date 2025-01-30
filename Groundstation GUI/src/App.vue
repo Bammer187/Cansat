531fd856afec2ad3cecc00b3e7ffd7ff328bb2da
Lukas Bammer <lukas.bammer@bulme.at>
2025-01-30 10:52:21 +0100

    Testing button

--- Groundstation GUI/src/App.vue
+++ Groundstation GUI/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-
+    <<button @click="count++">Count is: {{ count }}</button>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue';
+const count = ref(0);
 </script>
 
 <style scoped>
