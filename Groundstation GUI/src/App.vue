fb2d1048920b69ac571cd72ab1d4b5a6cf134b59
Lukas Bammer <lukas.bammer@bulme.at>
2025-01-30 21:09:10 +0100

    Deleted testing components

--- Groundstation GUI/src/App.vue
+++ Groundstation GUI/src/App.vue
@@ -1,10 +1,9 @@
 <template>
-    <<button @click="count++">Count is: {{ count }}</button>
+
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-const count = ref(0);
+
 </script>
 
 <style scoped>
