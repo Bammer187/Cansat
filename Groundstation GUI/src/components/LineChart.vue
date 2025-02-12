014cea5e0ea3482b393e9524f87ca2cbfd566d3c
Lukas Bammer <lukas.bammer@bulme.at>
2025-02-12 12:10:11 +0100

    Added imports

--- Groundstation GUI/src/components/LineChart.vue
+++ Groundstation GUI/src/components/LineChart.vue
@@ -3,5 +3,18 @@
 </template>
 
 <script lang="ts">
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+import { Line } from 'vue-chartjs';
+import type { ChartData } from 'chart.js';
+import { ref } from 'vue';
 
 </script>
