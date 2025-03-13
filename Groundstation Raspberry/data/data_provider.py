86e936e7b6069c49cb51b47d28b6bc3144864f93
Lukas Bammer <lukas.bammer@bulme.at>
2025-03-13 18:43:15 +0100

    ISSUE-025: Created functions in DataProvider class

--- Groundstation Raspberry/data/data_provider.py
+++ Groundstation Raspberry/data/data_provider.py
@@ -17,6 +17,12 @@ class DataProvider:
     
     def delete_last_24h(self):
         raise Exception("Method not yet implemented")
+    
+    def getAllDbEntries(self):
+        raise Exception("Method not yet implemented")
+    
+    def getNewestDbEntry(self):
+        raise Exception("Method not yet implemented")
 
     def open_connection(self, database):  
         raise Exception("Method not yet implemented")
